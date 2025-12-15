df4f792f2f61d47ca4a27df2eef037e63337d632
Asadullah Al Galib <assadulla0172@gmail.com>
2025-12-16 01:45:37 +0600

    Change the db.ts file add the create vehicles table query

--- src/database/db.ts
+++ src/database/db.ts
@@ -16,7 +16,20 @@ const initDB = async () => {
                 role VARCHAR(50) NOT NULL CHECK (role IN ('admin', 'user'))
             )
             
-        `)
+        `
+    )
+
+    await pool.query(`
+            CREATE TABLE IF NOT EXISTS vehicles(
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(250) NOT NULL,
+                type VARCHAR(50) NOT NULL CHECK (type IN ('car', 'bike', 'van', 'SUV')),
+                registration_number VARCHAR(150) UNIQUE NOT NULL,
+                daily_rent_price NUMERIC(10, 2) NOT NULL CHECK (daily_rent_price > 0),
+                availability_status VARCHAR(150) NOT NULL CHECK (availability_status IN ('available', 'booked'))
+            )
+        `
+    )
 }
 
 
